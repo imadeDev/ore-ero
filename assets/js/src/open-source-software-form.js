5a4c45385c02e7e1d01a27a9b1021fe9fcedaaac
Frédéric Lestage <frederic.lestage@ville.montreal.qc.ca>
2019-05-22 14:01:05 -0400

    Updated code for form handling

--- assets/js/src/open-source-software-form.js
+++ assets/js/src/open-source-software-form.js
@@ -5,35 +5,7 @@ function submitFormOSS() {
   resetButton.disabled = true;
   let content =
     '' +
-    `---
-schemaVersion: "1.0"
-description:
-  en: ${$('#enDescription').val()}
-  fr: ${$('#frDescription').val()}
-homepageURL:
-  en: ${$('#enHomepageURL').val()}
-  fr: ${$('#frHomepageURL').val()}
-licenses:
-  -
-    URL:
-      en: ${$('#enUrlLicense').val()}
-      fr: ${$('#frUrlLicense').val()}
-      spdxID: ${$('#spdxID').val()}
-name:
-  en: ${$('#enProjectName').val()}
-  fr: ${$('#frProjectName').val()}
-tags:
-  en:
-${[...document.querySelectorAll('#tagsEN input')]
-  .map(child => child.value)
-  .map(tag => '        - "' + tag + '"')
-  .join('\n')}
-  fr:
-${[...document.querySelectorAll('#tagsFR input')]
-  .map(child => child.value)
-  .map(tag => '        - "' + tag + '"')
-  .join('\n')}
-admininstrations:
+    `admininstrations:
   -
     adminCode: ${$('#adminCode').val()}
     uses:
@@ -50,77 +22,108 @@ admininstrations:
           en: ${$('#nameContact').val()}
           phone: ${$('#phoneContact').val()}
 `;
-  let file = `_data/software/${getSelectedOrgType()}/${$('#adminCode').val()}/${$('#enProjectName').val()}.yml`;
-
-  fetch(PRBOT_URL, {
-    body: JSON.stringify({
-      user: USERNAME,
-      repo: REPO_NAME,
-      title: 'Added software for ' + $('#adminCode :selected').text(),
-      description:
-        'Authored by: ' + $('#submitterEmail').val() + '\n' +
-        'Project: ***' + $('#enProjectName').val() + '***\n' +
-        $('#enDescription').val() + '\n',
-      commit: 'Commited bt ' + $('#submitterEmail').val(),
-      author: {
-        name: $('#submitterUsername').val(),
-        email: $('#submitterEmail').val()
-      },
-      files: [
-        {
-          path: file,
-          content: YAML.stringify(content, {keepBlobsInJSON: false })
-        }
-      ]
-    }),
-    method: 'POST'
-  })
-  .catch(err => {
-    if(err.status == 404) {
-      // We need to create the file for this organization, as it doesn't yet exist.
-      let header = `schemaVersion: "1.0"\nadminCode: ${$(
-        '#adminCode'
-      ).val()}\n`;
-      fetch(PRBOT_URL, {
+  let fileWriter = new YamlWriter(USERNAME, REPO_NAME);
+  let file = `_data/logiciels_libres-open_source_software/${$('#enProjectName').val()}.yml`;
+  fileWriter
+    .merge(file, content, 'admininstrations', 'name.en')
+    .then(result => {
+      const config = {
         body: JSON.stringify({
           user: USERNAME,
           repo: REPO_NAME,
-          title: 'Added software for ' + $('#adminCode :selected').text(),
+          title: 'Updated code for ' + $('#enProjectName').val(),
           description:
             'Authored by: ' + $('#submitterEmail').val() + '\n' +
             'Project: ***' + $('#enProjectName').val() + '***\n' +
             $('#enDescription').val() + '\n',
-          commit: 'Commited bt ' + $('#submitterEmail').val(),
+          commit: 'Commited by ' + $('submitterEmail').val(),
           author: {
-            name: $('#submitterUsername').val(),
-            email: $('#submitterEmail').val()
+            name: $('submitterUsername').val(),
+            email: $('submitterEmail').val()
           },
           files: [
             {
               path: file,
-              content: header + content
+              content: YAML.stringify(result, {keepBlobsInJSON: false})
             }
           ]
         }),
         method: 'POST'
-      });
-    } else {
-      throw err;
-    }
-  })
-  .then(response => {
-    if (response.status != 200) {
-      toggleAlert(ALERT_OFF);
-      toggleAlert(ALERT_FAIL);
-      submitButton.disabled = false;
-      resetButton.disabled = false;
-    } else {
-      toggleAlert(ALERT_OFF);
-      toggleAlert(ALERT_SUCCESS);
-      // redirect to home page
-      setTimeout(function {
-        window.location.href = './index.html';
-      }, 2000);
-    }
-  });
+      };
+      return fetch(PRBOT_URL, config);
+    })
+    .catch(err => {
+      if(err.status == 404) {
+        // We need to create the file for this project, as it doesn't yet exists.
+        let header = `---
+          schemaVersion: "1.0"
+          description:
+            en: ${$('#enDescription').val()}
+            fr: ${$('#frDescription').val()}
+          homepageURL:
+            en: ${$('#enHomepageURL').val()}
+            fr: ${$('#frHomepageURL').val()}
+          licenses:
+            -
+              URL:
+                en: ${$('#enUrlLicense').val()}
+                fr: ${$('#frUrlLicense').val()}
+              spdxID: ${$('#spdxID').val()}
+          name:
+            en: ${$('#enProjectName').val()}
+            fr: ${$('#frProjectName').val()}
+          tags:
+            en:
+          ${[...document.querySelectorAll('#tagsEN input')]
+            .map(child => child.value)
+            .map(tag => '        - "' + tag + '"')
+            .join('\n')}
+            fr:
+          ${[...document.querySelectorAll('#tagsFR input')]
+            .map(child => child.value)
+            .map(tag => '        - "' + tag + '"')
+            .join('\n')}`;
+        const config = {
+          body: JSON.stringify({
+            user: USERNAME,
+            repo: REPO_NAME,
+            title: 'Created software file for ' + $('#enProjectName').val(),
+            description:
+              'Authored by: ' + $('#submitterEmail').val() + '\n' +
+              'Project: ***' + $('#enProjectName').val() + '***\n' +
+              $('#enDescription').val() + '\n',
+            commit: 'Committed by ' + $('#submitterEmail').val(),
+            author: {
+              name: $('#submitterUsername').val(),
+              email: $('#submitterEmail').val()
+            },
+            files: [
+              {
+                path: file,
+                content: header + content
+              }
+            ]
+          }),
+          method: 'POST'
+        };
+        return fetch(PRBOT_URL, config);
+      } else {
+        throw err;
+      }
+    })
+    .then(response => {
+      if (response.status != 200) {
+        toggleAlert(ALERT_OFF);
+        toggleAlert(ALERT_FAIL);
+        submitButton.disabled = false;
+        resetButton.disabled = false;
+      } else {
+        toggleAlert(ALERT_OFF);
+        toggleAlert(ALERT_SUCCESS);
+        // Redirect to home page
+        setTimeout(function() {
+          window.location.href = './index.html';
+        }, 2000);
+      }
+    });
 }
