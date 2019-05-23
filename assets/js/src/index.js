16d2248dcd40efc03aec0a3ca5ad34252392462b
Imade Zarrari <46538305+imadeDev@users.noreply.github.com>
2019-05-23 14:39:59 -0400

    update index.js file

--- assets/js/src/index.js
+++ assets/js/src/index.js
@@ -65,62 +65,25 @@ function submitForm() {
   resetButton.disabled = true;
   let content =
     '' +
-    `releases:
-  -
-    contact: 
-      URL: 
-        en: ${$('#enUrlContact').val()}
-        fr: ${$('#frUrlContact').val()}
-    date: 
-      created: ${$('#dateCreated').val()}
-      metadataLastUpdated: ${$('#dateLastUpdated').val()}
-    description: 
-      en: ${$('#enDescription').val()}
-      fr: ${$('#frDescription').val()}
-    name: 
-      en: ${$('#enProjectName').val()}
-      fr: ${$('#frProjectName').val()}
-    licenses: 
-      - 
-        URL: 
-          en: ${$('#enUrlLicense').val()}
-          fr: ${$('#frUrlLicense').val()}
-          spdxID: ${$('#spdxID').val()}
-    repositoryURL: 
-      en: ${$('#enRepoUrl').val()}
-      fr: ${$('#frRepoUrl').val()}
-    status: ${$('#status :selected').text()}
-    tags: 
-      en: 
-${[...document.querySelectorAll('#tagsEN input')]
-  .map(child => child.value)
-  .map(tag => '        - "' + tag + '"')
-  .join('\n')}
-      fr: 
-${[...document.querySelectorAll('#tagsFR input')]
-  .map(child => child.value)
-  .map(tag => '        - "' + tag + '"')
-  .join('\n')}
-    vcs: ${$('#vcs').val()}
+    `- code: ${$('#adminCode').val()} 
+       created: "${$('#provinceCode').val()}"
+       name: 
+        en: ${$('#enAdminName').val()}
+        fr: ${$('#frAdminName').val()}
 `;
   let fileWriter = new YamlWriter(USERNAME, REPO_NAME);
-  let file = `_data/code/${getSelectedOrgType()}/${$('#adminCode').val()}.yml`;
+  let file = `_data/administrations/municipal.yml`;
   fileWriter
-    .merge(file, content, 'releases', 'name.en')
+    .merge(file, content, 'code', 'code')
     .then(result => {
       const config = {
         body: JSON.stringify({
           user: USERNAME,
           repo: REPO_NAME,
-          title: 'Updated code for ' + $('#adminCode :selected').text(),
+          title: 'Updated munucipal yaml file ',
           description:
             'Authored by: ' +
             $('#gitHubEmail').val() +
-            '\n' +
-            'Project: ***' +
-            $('#enProjectName').val() +
-            '***\n' +
-            $('#enDescription').val() +
             '\n',
           commit: 'Committed by ' + $('#gitHubEmail').val(),
           author: {
@@ -141,32 +104,24 @@ ${[...document.querySelectorAll('#tagsFR input')]
     .catch(err => {
       if (err.status == 404) {
         // We need to create the file for this organization, as it doesn't yet exist.
-        let header = `schemaVersion: "1.0"\nadminCode: ${$(
-          '#adminCode'
-        ).val()}\n`;
         const config = {
           body: JSON.stringify({
             user: USERNAME,
             repo: REPO_NAME,
-            title: 'Created code file for ' + $('#adminCode :selected').text(),
-            description:
-              'Authored by: ' +
-              $('#gitHubEmail').val() +
-              '\n' +
-              'Project: ***' +
-              $('#enProjectName').val() +
-              '***\n' +
-              $('#enDescription').val() +
-              '\n',
-            commit: 'Committed by ' + $('#gitHubEmail').val(),
-            author: {
-              name: $('#gitHubUsername').val(),
-              email: $('#gitHubEmail').val()
+            title: 'Updated munucipal yaml file ',
+          description:
+            'Authored by: ' +
+            $('#gitHubEmail').val() +
+            '\n',
+          commit: 'Committed by ' + $('#gitHubEmail').val(),
+          author: {
+            name: $('#gitHubUsername').val(),
+            email: $('#gitHubEmail').val()
             },
             files: [
               {
                 path: file,
-                content: header + content
+                content: content
               }
             ]
           }),
